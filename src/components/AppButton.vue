6664220938f0f6e5329126319be1c6ba50e80603
ivan-aim <77643196+ivan-aim@users.noreply.github.com>
2023-05-24 14:07:53 -0400

    Initial Commit

---
+++ src/components/AppButton.vue
@@ -0,0 +1,21 @@
+<template>
+  <div style="margin-top:20px;">First Element is a div</div>
+  <button type="button" v-bind="$attrs">
+    Button as 2nd element, must use v-bind $attrs to emit default click event
+  </button>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style scoped>
+div {
+  background-color: goldenrod;
+  border: 1px solid white;
+  border-radius: 5px;
+  margin-bottom: 10px;
+  padding: 10px;
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,38 @@
+<script setup>
+import { ref } from 'vue'
+import AppButton from './AppButton.vue'
+
+defineProps({
+  msg: String,
+})
+
+const count = ref(0)
+</script>
+
+<template>
+  <h1>{{ msg }}</h1>
+
+  <div class="card">
+    <button type="button" @click="count++">Click to increment count: {{ count }}</button>
+
+    <AppButton @click="count++" />
+  </div>
+</template>
+
+<style scoped>
+.card {
+  text-align: center;
+}
+
+:deep(button) {
+  background-color: lightgreen;
+  border: 1px solid darkgray;
+  color: darkgreen;
+  margin-bottom: 10px;
+}
+
+:deep(button:hover) {
+  background-color: darkgreen;
+  color: white;
+}
+</style>
